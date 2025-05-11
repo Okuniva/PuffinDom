26c8dcce834514f59b76ceef0bb9dbdc79e0dd54
Okuniva <AlexHeal@protonmail.com>
2025-05-11 11:53:22 +0300

    Replace user for UnverifiedDeviceTest

--- Samples/Bitwarden/Tests/Auth/Negative/UnverifiedDeviceTest.cs
+++ Samples/Bitwarden/Tests/Auth/Negative/UnverifiedDeviceTest.cs
@@ -1,5 +1,6 @@
 using Bitwarden.AppModels.Views;
 using Bitwarden.Helpers.Attributes;
+using Bitwarden.Models;
 using Bitwarden.TestLifecycle;
 using NUnit.Framework;
 using PuffinDom.Infrastructure.Helpers;
@@ -11,27 +12,26 @@ namespace Bitwarden.Tests.Auth.Negative;
 [Parallelizable]
 public class UnverifiedDeviceTest : UITestFixtureBase
 {
+    private readonly User _user = new("sicebid186@daupload.com", "NQcPU}wr]5q%!9W,L", "");
+
     [UITest("ToDo", RunOn.AndroidOnly_iOSInDevelopment)]
     public void TryLoginByUnverifiedDevice()
     {
-        const string email = "biveyin321@bamsrad.com"; // ToDo av replace
-        const string password = "M@sterP$$w0rd";
-
         using (var login = Pages.Login)
         {
-            login.EmailAddressEntry.EnterText(email);
+            login.EmailAddressEntry.EnterText(_user.Email);
             login.ContinueButton.Tap(screenClosed: true);
         }
 
         using (var masterPassword = Pages.MasterPassword)
         {
-            masterPassword.MasterPasswordEntry.EnterText(password);
+            masterPassword.MasterPasswordEntry.EnterText(_user.Password);
             masterPassword.LogInWithMasterPasswordButton.Tap(screenClosed: true);
         }
 
         using (var verifyYourIdentity = Pages.VerifyYourIdentity)
         {
-            verifyYourIdentity.NotRecognizeDescLabel(email).AssertExists();
+            verifyYourIdentity.NotRecognizeDescLabel(_user.Email).AssertExists();
             verifyYourIdentity.ContinueButton.AssertDisabled();
             verifyYourIdentity.ResendCodeButton.AssertEnabled();
         }
